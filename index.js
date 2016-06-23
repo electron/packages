825200d2616a5a354ebcf43d33ac0fc1eb5a9719
Zeke Sikelianos <zeke@sikelianos.com>
2016-06-23 14:44:37 -0700

    add error handler

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const ChangesStream = require('changes-stream')
 
 var changes = new ChangesStream({
-  db: 'https://skimdb.npmjs.com/registry',
+  db: 'https://fullfatdb.npmjs.com/registry',
   include_docs: true
 })
 
-changes.on('readable', function () {
-  var change = changes.read()
-  console.log('\n\n\n')
-  console.dir(change)
-})
+changes
+  .on('readable', function () {
+    var change = changes.read()
+    console.log('\n\n\n')
+    console.dir(change)
+  })
+  .on('error', function (err) {
+    console.error(err)
+  })
