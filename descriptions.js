b68201ffe476b42b2cdf11c2905e525d3a04c23a
Zeke Sikelianos <zeke@sikelianos.com>
2016-06-23 22:02:28 -0700

    add keywords to descriptions in README

--- descriptions.js
+++ descriptions.js
@@ -1,7 +1,13 @@
 const packages = require('.')
 
 Object.keys(packages).forEach(name => {
+  var pkg = packages[name]
+  var keywords
+  if (pkg.keywords) {
+    keywords = pkg.keywords.map(k => `_${k}_`).join(', ')
+    keywords = ` (${keywords})`
+  }
   console.log(
-    `- [${name}](http://npm.im/${name}) - ${packages[name].description} ([repo](http://ghub.io/${name}))`
+    `- [${name}](http://npm.im/${name}) - ${pkg.description}${keywords} ([repo](http://ghub.io/${name}))`
   )
 })
