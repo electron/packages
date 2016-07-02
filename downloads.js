3e3e70459dcc06827f189a1d5c67e246f4d88582
Zeke Sikelianos <zeke@sikelianos.com>
2016-07-01 22:41:09 -0700

    get download counts

--- downloads.js
+++ downloads.js
@@ -1,4 +1,10 @@
 const got = require('got')
+const fs = require('fs')
+const path = require('path')
+const exists = require('path-exists').sync
+const packageNames = require('.').map(p => p.name)
+const RateLimiter = require('limiter').RateLimiter
+const limiter = new RateLimiter(1, 1000)
 
 function getDownloadCount(pkg, callback) {
   const url = `https://api.npmjs.org/downloads/point/last-month/${pkg}`
@@ -7,6 +13,13 @@ function getDownloadCount(pkg, callback) {
     .catch(error => { callback(error) })
 }
 
-getDownloadCount('express', function(err, count) {
-  console.log(count)
+packageNames
+  .filter(name => !exists(path.join(__dirname, `./downloads/${name}.json`)))
+  .forEach(name => {
+    limiter.removeTokens(1, function() {
+      getDownloadCount(name, function(err, count) {
+        console.log(`${name}: ${count}`)
+        fs.writeFileSync(path.join(__dirname, `./downloads/${name}.json`), String(count))
+      })
+  })
 })
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('object-values')(require('require-dir')('./packages'))
+const fs = require('fs')
+const path = require('path')
+
+const exists = require('path-exists').sync
+var packages = require('object-values')(require('require-dir')('./packages'))
+
+
+packages.forEach(p => {
+  var file = path.join(__dirname, `./downloads/${p.name}.json`)
+  if (exists(file)) {
+    p.downloadsInLastMonth = Number(fs.readFileSync(file, 'utf8'))
+  }
+})
+
+
+module.exports = packages
